3b4d71ad0553cfad10bb69028ed00df7dbbe96a4
Kazuhito Hokamura <k.hokamura@gmail.com>
2021-07-15 23:56:14 +0900

    Introduce codemirror

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,7 +1,36 @@
+import dynamic from "next/dynamic";
+import "codemirror/lib/codemirror.css";
+import type { EditorConfiguration } from "codemirror";
+import { useState } from "react";
+
+const CodeMirror = dynamic(
+  async () => {
+    await import("codemirror/mode/markdown/markdown");
+    await import("codemirror/mode/javascript/javascript");
+    await import("codemirror/keymap/vim");
+
+    const codemirror = await import("react-codemirror2");
+    return codemirror.UnControlled;
+  },
+  { ssr: false }
+);
+
 const IndexPage = () => {
+  const [mode, setMode] = useState("markdown");
+  const handleClickMode = () => {
+    setMode(mode === "markdown" ? "javascript" : "markdown");
+  };
+  const options: EditorConfiguration = {
+    mode,
+    theme: "vscode-dark",
+    keyMap: "vim",
+  };
   return (
-    <div id="layout">
-      <textarea id="editor"></textarea>
+    <div id="editor">
+      <CodeMirror options={options}></CodeMirror>
+      <div id="toolbar">
+        <span onClick={handleClickMode}>{mode}</span>
+      </div>
     </div>
   );
 };
